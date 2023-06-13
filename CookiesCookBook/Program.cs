f637193801b3fe3e4c135ea1d68ab58cc7f5c1df
michaeljayc <mjcaballero.dev@gmail.com>
2023-06-13 18:02:11 -0400

    added application main workflow and interfaces

--- CookiesCookBook/Program.cs
+++ CookiesCookBook/Program.cs
@@ -1,4 +1,6 @@
-﻿var app = new CookieRecipeApp(new CookieRepository(), new UserConsoleInteraction());
+﻿using CookiesCookBook.Recipe.Ingredients;
+
+var app = new CookieRecipeApp(new CookieRepository(), new UserConsoleInteraction());
 app.Run();
 
 
@@ -13,28 +15,72 @@ public class CookieRecipeApp
     }
     public void Run()
     {
-
+        var savedRecipes = _cookieRepository.Read();
+        _userConsoleInteraction.PrintAvailableIngredients();
+        var newIngredients = _userConsoleInteraction.AddNewIngredients();
+        if(newIngredients.Count() > 0)
+        {
+            _cookieRepository.Write(newIngredients);
+        }
+        else
+        {
+            _userConsoleInteraction.PrintMessage("No ingredients have been saved. Recipe will not be saved.");
+            _userConsoleInteraction.Exit();
+        }
     }
-
 
 }
 
 public class CookieRepository : ICookieRepository
 {
+    public string Read()
+    {
+        return "";
+    }
 
+    public void Write(IEnumerable<Ingredient> ingredients)
+    {
+
+    }
 }
 
 public class UserConsoleInteraction : IUserInteraction
 {
+    public void PrintAvailableIngredients()
+    {
+
+    }
+
+    public IEnumerable<Ingredient> AddNewIngredients()
+    {
+        var ingredients = new List<Ingredient>()
+        { new Wheat(), new Sugar() };
+        return ingredients;
+    }
+
+    public void PrintMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+    public void Exit()
+    {
+        Console.WriteLine("Press any key to continue.");
+        Console.ReadKey();
+    }
 
 }
 
 public interface ICookieRepository
 {
 
+    public void Write(IEnumerable<Ingredient> ingredients);
+    public string Read();
 }
 
 public interface IUserInteraction
 {
-    
+    public void PrintAvailableIngredients();
+    public IEnumerable<Ingredient> AddNewIngredients();
+    public void PrintMessage(string message);
+    public void Exit();
 }
---
+++ CookiesCookBook/Recipe/Recipe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookiesCookBook.Recipe
+{
+    internal class Recipe
+    {
+    }
+}
